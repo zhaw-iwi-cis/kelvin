e7bd2fd2112e20a32b9772db7edc00c2695a1715
Robert Erdin <erdi@zhaw.ch>
2014-09-18 12:27:32 +0200

    overriding merge documentation

--- src/main/java/ch/zhaw/iwi/cis/kelvin/dao/IdentifiableObjectDao.java
+++ src/main/java/ch/zhaw/iwi/cis/kelvin/dao/IdentifiableObjectDao.java
@@ -9,6 +9,34 @@ public interface IdentifiableObjectDao
 {
 	public < T extends IdentifiableObject > void persist( T object );
 	public < T extends IdentifiableObject > void remove( T object );
+
+    /**
+     * <b>CAREFUL when overriding!</b> {@link javax.persistence.EntityManager}.merge does not add detached objects to
+     * the persistence context, but either returns the existing object currently present in the persistence context or a
+     * new one if it was not there at the time of the merge.
+     * From <a href="http://www.objectdb.com/java/jpa/persistence/detach#Explicit_Merge_">objectdb.com</a>: <br>
+     * The content of the specified detached entity object is copied into an existing managed entity object with the same
+     * identity (i.e. same type and primary key). If the EntityManager does not manage such an entity object yet a new
+     * managed entity object is constructed. The detached object itself, however, remains unchanged and detached.
+     * <br><br>
+     *
+     * <b>Example overriding:</b><br>
+     * public <T extends IdentifiableObject> T merge(T object) {
+     *      Company company = (Company)object;
+     *      List<RawObject> managed = new ArrayList<RawObject>();
+     *      for (RawObject ro : company.getRawObjects()) {
+     *          managed.add(getEntityManager().merge(ro));
+     *      }
+     *      company.getRawObjects().clear();
+     *      company.getRawObjects().addAll(managed);
+     *
+     *      return (T)company;
+     * }
+     *
+     * @param object
+     * @param <T>
+     * @return
+     */
 	public < T extends IdentifiableObject > T merge( T object );
 	public < T extends IdentifiableObject > T findById( ObjectID id );
 	public < T extends IdentifiableObject > List< T > findByAll(Class< T > clazz);
